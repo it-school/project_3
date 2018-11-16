bf904efbb00d3d593aa6445897b1f1848ef14fd5
Nikita <kirsh321@gmail.com>
2018-11-16 22:14:00 +0200

    commit 4

--- src/Main.java
+++ src/Main.java
@@ -6,7 +6,7 @@ public class Main {
         double x3 = 0.25;
         double x4 = 88.675;
 
-        System.out.printf("sqrt(%.hijrthiljrti = %.3f%n", x1, Math.sqrt(x1));
+        System.out.printf("sqrt(%.3f) = %.3f%n", x1, Math.sqrt(x1));
         System.out.printf("sqrt(%.3f) = %.3f%n", x2, Math.sqrt(x2));
         System.out.printf("sqrt(%.3f) = %.3f%n", x3, Math.sqrt(x3));
         System.out.printf("sqrt(%.3f) = %.3f%n", x4, Math.sqrt(x4));
