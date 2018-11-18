8746d377af747e8f2002091a6161035dc22fef98
Nikita <kirsh321@gmail.com>
2018-11-18 18:11:06 +0200

    project#2

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,32 @@
 public class Main {
 
     public static void main(String[] args) {
-        double x1 = 16;
-        double x2 = 2.25;
-        double x3 = 0.25;
-        double x4 = 88.675;
 
-        System.out.printf("sqrt(%.3f) = %.3f%n", x1, Math.sqrt(x1));
-        System.out.printf("sqrt(%.3f) = %.3f%n", x2, Math.sqrt(x2));
-        System.out.printf("sqrt(%.3f) = %.3f%n", x3, Math.sqrt(x3));
-        System.out.printf("sqrt(%.3f) = %.3f%n", x4, Math.sqrt(x4));
+        double a = 5;
+        double b = 3.5;
+        double c = 0.25;
+
+        if (a > 0) ;
+        double x = ((b + Math.sqrt(Math.pow(b, 2)) + 4 * a * c) / 2 * a) - (Math.pow(a, 3) * c) + Math.pow(b, -2);
+        System.out.println("X =  " + x);
+
+        double s = 154;
+        double q = Math.pow(s, 2) + Math.pow(s, 3);
+        System.out.println("Q =  " + q);
+
+        double m = 159;
+        double t = m -(Math.pow(q, 3)/3)+(Math.pow(m, 5)/5);
+        System.out.println("T =  " + t);
+
+        double n = 5, v =4;
+        double k = ((n + v) / (v+1))-((n*v)-12)/(34+n);
+       if (n > 0) if( v > 0);
+        System.out.println("K =  " + k);
+
     }
+
+
+
 }
+
+
